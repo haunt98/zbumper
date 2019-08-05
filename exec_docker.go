2af4c2c414e4df0cd26120aef022e79824df8bdd
Quang Le <phuc.quang102@gmail.com>
2019-08-05 11:00:52 +0700

    make docker build output to screen

--- exec_docker.go
+++ exec_docker.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"os/exec"
 	"strings"
@@ -14,10 +16,33 @@ func buildAndDeployDocker(b *Bump, accessToken string) (string, error) {
 	imageTag := fmt.Sprintf("%s:v%s", repo.Location, b.Version)
 	dockerfileName := fmt.Sprintf("%s/docker/Dockerfile_%s", b.Project, b.Service)
 	dockerHost := strings.Split(repo.Location, "/")[0]
-	output, err := exec.Command(
+	cmdStr := strings.Join([]string{
 		"docker", "build", "-t", imageTag, "-f", dockerfileName, ".", "&&",
 		"docker", "login", dockerHost, "&&",
-		"docker", "push", imageTag).
-		CombinedOutput()
-	return string(output), err
+		"docker", "push", imageTag},
+		" ")
+
+
+	cmd := exec.Command("bash", "-c", cmdStr)
+
+
+	cmdReader, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	scanner := bufio.NewScanner(cmdReader)
+	go func() {
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+	if err := cmd.Start(); err != nil {
+		err = errors.New("cannot start command: " + err.Error())
+		return "", err
+	}
+	if err := cmd.Wait(); err != nil {
+		err = errors.New("cannot wait command: " + err.Error())
+		return "", err
+	}
+	return string("success"), err
 }
